536e2464ecfeffe1924331fee2e026fbbb7c69c9
SebaMoya92 <sebamoya92@gmail.com>
2023-07-24 19:47:41 -0300

    agrego para que se puedan ver los videos

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,6 +24,7 @@ async function fetchData(urlApi) {
     ${videos.items
       .map(
         (video) => `
+        <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
     <div class="group relative">
     <div
       class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
